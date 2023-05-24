e3ed2c43b40bbae454f0dec1d4f8c29da1ac3333
samcyn <samsoniyanda@outlook.com>
2023-05-24 17:24:45 +0100

    style: refactor app-link component

--- components/shared/AppLink.vue
+++ components/shared/AppLink.vue
@@ -1,28 +1,5 @@
-<script setup lang="ts">
-// import type { NuxtLinkProps } from 'nuxt/app';
-import type { RouteLocationRaw } from '#vue-router';
-
-interface Props {
-  to?: RouteLocationRaw;
-  href?: RouteLocationRaw;
-  external?: boolean;
-  replace?: boolean;
-  custom?: boolean;
-  target?: '_blank' | '_parent' | '_self' | '_top' | (string & {}) | null;
-  rel?: string | null;
-  noRel?: boolean;
-  prefetch?: boolean;
-  noPrefetch?: boolean;
-  activeClass?: string;
-  exactActiveClass?: string;
-  ariaCurrentValue?: string;
-}
-// set defaults props
-const props = withDefaults(defineProps<Props>(), {});
-
+<script lang="ts">
+export default defineNuxtLink({
+  componentName: 'AppNuxtLink',
+});
 </script>
-<template>
-  <NuxtLink v-bind="props">
-    <slot />
-  </NuxtLink>
-</template>
