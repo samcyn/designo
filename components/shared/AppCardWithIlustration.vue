499853bc891b89d4bfb916ac23454662ad68ef4a
samcyn <samsoniyanda@outlook.com>
2023-05-25 14:15:24 +0100

    feat: add card with illustration

---
+++ components/shared/AppCardWithIlustration.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+interface Props {
+	matrix?: "octa" | "semi" | "deca";
+	title: string;
+	description: string;
+	src?: string;
+	alt?: string;
+}
+
+// set defaults props
+withDefaults(defineProps<Props>(), {
+	matrix: "semi",
+});
+</script>
+<template>
+	<div
+		class="flex flex-col md:flex-row xl:flex-col items-center justify-center gap-12"
+	>
+		<figure
+			class="card__figure flex justify-center items-center relative overflow-hidden"
+		>
+			<slot class="z-10 relative">
+				<img
+					:src="src"
+					:alt="alt"
+					class="z-10 relative"
+				/>
+			</slot>
+			<div
+				class="card__oval absolute inset-0 w-full h-full"
+				:class="`${matrix}-matrix`"
+			></div>
+		</figure>
+		<div class="md:grow md:shrink md:basis-0">
+			<p
+				class="card__title text-dark-grey text-center md:text-left xl:text-center font-normal mb-8 md:mb-4 xl:mb-8"
+			>
+				{{ title }}
+			</p>
+			<p
+				class="card__description text-dark-grey font-normal text-center md:text-left xl:text-center"
+			>
+				{{ description }}
+			</p>
+		</div>
+	</div>
+</template>
+<style scoped lang="css">
+.card__figure {
+	width: 202px;
+	flex: 0 0 202px;
+	height: 202px;
+}
+.card__oval {
+	border-radius: 100%;
+	background: linear-gradient(
+		90deg,
+		rgba(93, 2, 2, 0.0001) 0%,
+		rgba(93, 2, 2, 0.497569) 100%
+	);
+	mix-blend-mode: normal;
+	opacity: 0.2;
+	/* transform: matrix(0, -1, -1, 0, 0, 0);
+  transform: matrix(-1, 0, 0, 1, 0, 0);
+	transform: matrix(0, 1, 1, 0, 0, 0); */
+}
+.semi-matrix {
+	transform: matrix(0, -1, -1, 0, 0, 0);
+}
+.octa-matrix {
+	transform: matrix(0, 1, 1, 0, 0, 0);
+}
+.deca-matrix {
+	transform: matrix(-1, 0, 0, 1, 0, 0);
+}
+.card__title {
+	font-size: 20px;
+	line-height: 26px;
+	letter-spacing: 5px;
+}
+.card__description {
+	font-size: 16px;
+	line-height: 26px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AppHero from "~/components/shared/AppHero.vue";
 import AppCardWithOverlay from "~/components/shared/AppCardWithOverlay.vue";
+import AppCardWithIlustration from "~/components/shared/AppCardWithIlustration.vue";
 </script>
 <template>
 	<app-hero />
@@ -15,22 +16,22 @@ import AppCardWithOverlay from "~/components/shared/AppCardWithOverlay.vue";
 					title="Web design"
 					sub-title="view projects"
 				>
-          <img
-            src="~/assets/home/mobile/image-web-design.jpg"
-            alt="alt"
-            class="object-cover w-full h-full md:hidden"
-          />
-          <img
-            src="~/assets/home/tablet/image-web-design.jpg"
-            alt="alt"
-            class="object-cover w-full h-full hidden md:block xl:hidden"
-          />
-          <img
-            src="~/assets/home/desktop/image-web-design-large.jpg"
-            alt="alt"
-            class="object-cover w-full h-full hidden md:hidden xl:block"
-          />
-        </AppCardWithOverlay>
+					<img
+						src="~/assets/home/mobile/image-web-design.jpg"
+						alt="alt"
+						class="object-cover w-full h-full md:hidden"
+					/>
+					<img
+						src="~/assets/home/tablet/image-web-design.jpg"
+						alt="alt"
+						class="object-cover w-full h-full hidden md:block xl:hidden"
+					/>
+					<img
+						src="~/assets/home/desktop/image-web-design-large.jpg"
+						alt="alt"
+						class="object-cover w-full h-full hidden md:hidden xl:block"
+					/>
+				</AppCardWithOverlay>
 			</div>
 
 			<div
@@ -42,51 +43,72 @@ import AppCardWithOverlay from "~/components/shared/AppCardWithOverlay.vue";
 							class="w-full"
 							to="/app-design"
 							title="App design"
-					    sub-title="view projects"
+							sub-title="view projects"
 						>
-              <img
-                src="~/assets/home/mobile/image-web-design.jpg"
-                alt="alt"
-                class="object-cover w-full h-full md:hidden"
-              />
-              <img
-                src="~/assets/home/tablet/image-web-design.jpg"
-                alt="alt"
-                class="object-cover w-full h-full hidden md:block xl:hidden"
-              />
-              <img
-                src="~/assets/home/desktop/image-web-design-large.jpg"
-                alt="alt"
-                class="object-cover w-full h-full hidden md:hidden xl:block"
-              />
-            </AppCardWithOverlay>
+							<img
+								src="~/assets/home/mobile/image-web-design.jpg"
+								alt="alt"
+								class="object-cover w-full h-full md:hidden"
+							/>
+							<img
+								src="~/assets/home/tablet/image-web-design.jpg"
+								alt="alt"
+								class="object-cover w-full h-full hidden md:block xl:hidden"
+							/>
+							<img
+								src="~/assets/home/desktop/image-web-design-large.jpg"
+								alt="alt"
+								class="object-cover w-full h-full hidden md:hidden xl:block"
+							/>
+						</AppCardWithOverlay>
 					</div>
 					<div class="basis-full p-3">
 						<AppCardWithOverlay
 							class="w-full"
 							to="/graphic-design"
 							title="Graphic design"
-					    sub-title="view projects"
+							sub-title="view projects"
 						>
-              <img
-                src="~/assets/home/mobile/image-web-design.jpg"
-                alt="alt"
-                class="object-cover w-full h-full md:hidden"
-              />
-              <img
-                src="~/assets/home/tablet/image-web-design.jpg"
-                alt="alt"
-                class="object-cover w-full h-full hidden md:block xl:hidden"
-              />
-              <img
-                src="~/assets/home/desktop/image-web-design-large.jpg"
-                alt="alt"
-                class="object-cover w-full h-full hidden md:hidden xl:block"
-              />
-            </AppCardWithOverlay>
+							<img
+								src="~/assets/home/mobile/image-web-design.jpg"
+								alt="alt"
+								class="object-cover w-full h-full md:hidden"
+							/>
+							<img
+								src="~/assets/home/tablet/image-web-design.jpg"
+								alt="alt"
+								class="object-cover w-full h-full hidden md:block xl:hidden"
+							/>
+							<img
+								src="~/assets/home/desktop/image-web-design-large.jpg"
+								alt="alt"
+								class="object-cover w-full h-full hidden md:hidden xl:block"
+							/>
+						</AppCardWithOverlay>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
+	<div class="container">
+		<app-card-with-ilustration
+			title="FRIENDLY"
+			description="
+        We are a group of enthusiastic folks 
+        who know how to put people first. 
+        Our success depends on our customers, 
+        and we strive to give them the 
+        best experience a company can provide.
+      "
+      matrix="semi"
+		>
+      <template v-slot="imgProps">
+        <img 
+          src="~/assets/home/desktop/illustration-friendly.svg"
+          alt="illustrstions"
+          v-bind="imgProps"
+        />
+      </template>
+    </app-card-with-ilustration>
 	</div>
 </template>
