05cfa630fca37dcb12213e3a9fed5a37be0babc9
samcyn <samsoniyanda@outlook.com>
2023-05-29 14:37:56 +0100

    feat: add location page and components

---
+++ components/shared/AppCardWithMap.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import { VNodeRef } from "nuxt/dist/app/compat/capi";
+
+interface Props {
+	title: string;
+  reverse?: boolean
+  latitude: number
+  longitude: number
+}
+const mapRef: VNodeRef | undefined = ref();
+
+const props = withDefaults(defineProps<Props>(), {});
+const { $leaflet } = useNuxtApp();
+
+const {
+  latitude,
+  longitude
+} = toRefs(props);
+
+onMounted(() => {
+	if (mapRef.value && latitude.value && longitude.value) {
+		const map = $leaflet
+			.map(mapRef.value)
+			.setView([latitude.value, longitude.value], 13);
+		$leaflet
+			.tileLayer(
+				"https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+				{
+					maxZoom: 19,
+					attribution:
+						'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+				}
+			)
+			.addTo(map);
+	}
+});
+</script>
+<template>
+	<div 
+    class="card flex flex-col justify-center items-center md:gap-6 xl:gap-30px"
+    :class="reverse ? 'xl:flex-row-reverse' : 'xl:flex-row'"
+  >
+		<!-- map -->
+		<div
+			class="card__map w-full h-80 overflow-hidden -z-1"
+			ref="mapRef"
+		></div>
+
+		<div
+			class="card__body w-full text-center md:text-left px-6 py-20 bg-pale xl:flex-1"
+		>
+			<p
+				class="card__title text-peach font-medium mb-6"
+			>
+				{{ title }}
+			</p>
+			<ul
+				class="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-30px"
+			>
+				<li
+					class="card__description text-dark-grey"
+				>
+					<slot name="address">
+						<span class="font-bold"
+							>Designo Central Office</span
+						><br />
+						3886 Wellington Street <br />Toronto,
+						Ontario M9C 3J5
+					</slot>
+				</li>
+				<li
+					class="card__description text-dark-grey"
+				>
+					<slot name="contact">
+						<span class="font-bold">Contact</span
+						><br />
+						P: +1 253-863-8967<br />
+						M: contact@designo.co
+					</slot>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+<style scoped lang="css">
+.card__title {
+	font-size: 32px;
+	line-height: 36px;
+}
+.card__description {
+	font-size: 15px;
+	line-height: 25px;
+}
+
+@media (min-width: 768px) {
+	.card__body,
+	.card__map {
+		border-radius: 15px;
+	}
+	.card__body {
+		padding: 88px 74px;
+	}
+	.card__title {
+		font-size: 40px;
+		line-height: 48px;
+	}
+	.card__description {
+		font-size: 16px;
+		line-height: 26px;
+	}
+}
+@media (min-width: 1440px) {
+  .card__map {
+    width: 350px;
+  }
+}
+</style>
--- pages/locations/index.vue
+++ pages/locations/index.vue
@@ -1,1 +1,72 @@
-<template>Location</template>
+<script setup lang="ts">
+import AppCardWithMap from "~/components/shared/AppCardWithMap.vue";
+</script>
+<template>
+	<div class="container">
+		<div class="-mx-6 md:mx-0 mb-30 xl:mb-40">
+			<div
+				class="grid grid-cols-1 gap-10 md:gap-30 xl:gap-8"
+			>
+				<app-card-with-map
+					title="Canada"
+					:latitude="43.648233"
+					:longitude="-79.375602"
+					reverse
+				>
+					<template #address>
+						<span class="font-bold"
+							>Designo Central Office</span
+						><br />
+						3886 Wellington Street <br />Toronto,
+						Ontario M9C 3J5
+					</template>
+					<template #contact>
+						<span class="font-bold">Contact</span
+						><br />
+						P: +1 253-863-8967<br />
+						M: contact@designo.co
+					</template>
+				</app-card-with-map>
+				<app-card-with-map
+					title="Australia"
+					:latitude="-30.32953"
+					:longitude="149.78819"
+				>
+					<template #address>
+						<span class="font-bold"
+							>Designo Central Office</span
+						><br />
+						19 Balonne Street<br />New South Wales
+						2443
+					</template>
+					<template #contact>
+						<span class="font-bold">Contact</span
+						><br />
+						P: (02) 6720 9092<br />
+						M: contact@designo.co
+					</template>
+				</app-card-with-map>
+				<app-card-with-map
+					title="United Kingdom"
+					:latitude="51.14153"
+					:longitude="1.29764"
+					reverse
+				>
+					<template #address>
+						<span class="font-bold"
+							>Designo Central Office</span
+						><br />
+						13 Colorado Way<br />Rhyd-y-fro SA8
+						9GA
+					</template>
+					<template #contact>
+						<span class="font-bold">Contact</span
+						><br />
+						P: 078 3115 1400<br />
+						M: contact@designo.co
+					</template>
+				</app-card-with-map>
+			</div>
+		</div>
+	</div>
+</template>
