a7c66207d5219c03dcf2e8c62191fece658b729f
samcyn <samsoniyanda@outlook.com>
2023-05-31 19:54:29 +0100

    feat: added transition css

--- components/contact-page/AppContactForm.vue
+++ components/contact-page/AppContactForm.vue
@@ -1,44 +1,43 @@
 <script setup lang="ts">
-import { Events } from 'vue';
+import { Events } from "vue";
 import AppInput from "~/components/shared/AppInput.vue";
 import AppButton from "~/components/shared/AppButton.vue";
 
 type FormType = {
-  name: boolean;
-  email: boolean;
-  phone: boolean;
-  message: boolean;
-}
+	name: boolean;
+	email: boolean;
+	phone: boolean;
+	message: boolean;
+};
 const errorBag = reactive({
-  name: false,
-  email: false,
-  phone: false,
-  message: false
+	name: false,
+	email: false,
+	phone: false,
+	message: false,
 });
 
 const form = reactive({
 	name: "",
 	email: "",
-  phone: '',
-  message: ''
+	phone: "",
+	message: "",
 });
 
-const onSubmit = (e: Events['onSubmit']) => {
-  e.preventDefault();
+const onSubmit = (e: Events["onSubmit"]) => {
+	e.preventDefault();
 	for (const key in form) {
 		if (!form[key as keyof FormType]) {
 			errorBag[key as keyof FormType] = true;
 		}
 	}
-  console.log('submitting...')
-}
-
+	console.log("submitting...");
+};
 </script>
 <template>
 	<div class="container">
 		<div class="-mx-6 md:mx-0">
 			<div
-				class="relative overflow-hidden bg-peach py-18 px-6 md:px-[58px] xl:py-[54px] xl:px-24 md:rounded-[15px] xl:flex xl:gap-[95px] xl:items-center"
+				class="relative overflow-hidden bg-peach py-18 px-6 md:px-[58px] xl:py-[54px] xl:px-24 md:rounded-[15px] xl:flex xl:gap-[95px] xl:items-center transition group"
 			>
 				<div
 					class="text-center md:text-left text-white xl:flex-1"
@@ -61,32 +60,32 @@ const onSubmit = (e: Events['onSubmit']) => {
 					</p>
 				</div>
 				<form
-          @submit="onSubmit"
+					@submit="onSubmit"
 					class="mt-6 md:mt-4 xl:mt-0 xl:w-[380px]"
 				>
 					<div
 						class="flex flex-col mb-10 md:mb-6"
 					>
 						<AppInput
-						  v-model="form.name"
+							v-model="form.name"
 							name="name"
 							variant="custom"
 							placeholder="Name"
-              :has-error="errorBag.name"
+							:has-error="errorBag.name"
 						/>
 						<AppInput
 							v-model="form.email"
 							name="email"
 							variant="custom"
 							placeholder="Email"
-              :has-error="errorBag.email"
+							:has-error="errorBag.email"
 						/>
 						<AppInput
 							v-model="form.phone"
 							name="phone"
 							variant="custom"
 							placeholder="Phone"
-              :has-error="errorBag.phone"
+							:has-error="errorBag.phone"
 						/>
 						<AppInput
 							v-model="form.message"
@@ -107,7 +106,7 @@ const onSubmit = (e: Events['onSubmit']) => {
 				</form>
 				<!-- overlays -->
 				<div
-					class="hidden md:block overlay absolute md:-top-[83px] md:-left-[121px] xl:left-0 xl:-top-40"
+					class="hidden md:block overlay overlaysApp__circle absolute md:-top-[83px] md:-left-[121px] xl:left-0 xl:-top-40 transition group-hover:scale-105"
 				></div>
 				<div
 					class="md:hidden w-[584px] absolute grid grid-cols-2 transition group-hover:translate-x-2 -left-[92px] top-0"
@@ -123,3 +122,9 @@ const onSubmit = (e: Events['onSubmit']) => {
 		</div>
 	</div>
 </template>
+<style scoped lang="css">
+.overlay {
+	width: 640px;
+	height: 640px;
+}
+</style>
